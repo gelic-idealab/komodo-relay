0bcfb941ada13396843658153d8864e0e06681b8
unknown <rwwalla2@illinois.edu>
2020-12-07 15:39:43 -0600

    traefik host rule template, remove refs to rsync

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,5 @@ FROM node:latest
 COPY . /komodo
 WORKDIR /komodo
 RUN npm install
-RUN apt-get update && apt-get install rsync -y
 CMD [ "node", "serve.js"]
 EXPOSE 3000
--- sync_session_files.sh
+++
@@ -1,4 +0,0 @@
-# Synchronizes session files between relay and data servers:
-# we can run this script without a password because we have generated a private/public keypair to the komodo-ml VM
-# we authenticate first using the key and then rysync command is executed
-rsync -avz --exclude='.mic' ./captures/ graingeridealab@13.67.142.168:/home/graingeridealab/komodo_data/captures/
